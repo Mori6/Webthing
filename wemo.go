0c031d45c4a7ed2b4c93097c4e6e6fde61daffeb
mori <m>
2019-10-26 20:19:46 -0700

    wm

--- wemo.go
+++ wemo.go
@@ -1,3 +1,4 @@
+//golang implementation of belkin wemo upnp RCE
 package wemo
 
 import (
@@ -5,7 +6,40 @@ import (
 
 )
 
-func wemo() { 
+func is_vulnerable(ip string, port string) bool { 
+	var vuln_paths[] string = "/setupservice.xml", "/firmwareupdate.xml", "/remoteaccess.xml", "/setup.xml"
+	for _, i := range vuln_paths {	
+		url := ar request string = "http://" + "ip" + ":" + port + i
+		client := &http.Client{}
+		sRequestContent := generateRequestContent(postalCode)
+		requestContent := []byte(sRequestContent)
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestContent))
+		if err != nil {
+			return nil,err
+		}
 	
+		req.Header.Add("SOAPAction", url)
+		req.Header.Add("Content-Type", "text/xml; charset=utf-8")
+		req.Header.Add("Accept", "text/xml")
+		resp, err := client.Do(req)
+		if err != nil {
+			return nil,err
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != 200 {
+			return nil, errors.New("Error Respose " + resp.Status)
+		}
+		contents, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+}
+}
+
+func wemo() { 
+
+
+
 
 }
